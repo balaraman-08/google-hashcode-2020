1fe81d0db9b0279ca3db042705e2d9aef5b2f0dc
Balaraman M <balaramanmuthupandi@gmail.com>
2020-02-23 11:24:32 +0530

    Change sort function
    Instead of adding the factors, multiplied the factors.

--- solution.py
+++ solution.py
@@ -7,7 +7,7 @@ f = open(f'{fileName}.txt')
 
 scoreFile = open("scores.txt", "r+")
 score = int(scoreFile.readline())
-scoreFile.seek(0, os.SEEK_SET) 
+scoreFile.seek(0, os.SEEK_SET)
 
 nBooks, nLibraries, nDays = map(int, f.readline().split())
 bookScores = list(map(int, f.readline().split()))
@@ -20,7 +20,8 @@ for i in range(nLibraries):
     bookList.sort(key=lambda book: bookScores[book], reverse=True)
     sumOfBookScores = sum([bookScores[book] for book in bookList])
     avgOfBookScores = sumOfBookScores/nBooksinL
-    booksOverAverageScore = [book for book in bookList if bookScores[book] >= avgOfBookScores]
+    booksOverAverageScore = [
+        book for book in bookList if bookScores[book] >= avgOfBookScores]
     libraries.append(
         {
             "id": i,
@@ -29,14 +30,41 @@ for i in range(nLibraries):
             "shippingPerDay": shippingPerDay,
             "bookList": bookList,
             'sumOfBookScores': sumOfBookScores,
-            'booksOverAverageScore': booksOverAverageScore
+            'booksOverAverageScore': booksOverAverageScore,
+            'uniqueBooks': []
         }
     )
 
+# Previoulsy added the factors but that wasn't right approach
+# since all the factors are of unit
+""" 
 libraries.sort(key=lambda x: x['signupDays'] + 1/x['sumOfBookScores'] +
                (1/x['nBooks'])+(0.0025/x['shippingPerDay'])+(0.00025/len(x['booksOverAverageScore'])))
+ """
 
-# libraries.sort(key=lambda x: 0.9*x['signupDays'] + 1/x['sumOfBookScores'] + 0.0001*x['nBooks']/x['shippingPerDay'])
+# Rewrote it as multiplicative factors based on a friend's idea
+# Sorting libraries based on signupDays required and total value of books stored in the library
+# Libraries with less signupDays and high value of books will be at first
+libraries.sort(key=lambda x: x['signupDays'] / (x['sumOfBookScores']
+                                                * x['shippingPerDay']))
+
+
+# Keeping only unique books in the library
+isBookAlreadyPresent = [False] * nBooks
+
+for library in libraries:
+    for book in library['bookList']:
+        if not isBookAlreadyPresent[book]:
+            isBookAlreadyPresent[book] = True
+            library['uniqueBooks'].append(book)
+
+    library['sumOfUniqueBookScores'] = sum([bookScores[book]
+                                            for book in library['uniqueBooks']])
+    if library['sumOfUniqueBookScores'] == 0:
+        library['sumOfUniqueBookScores'] = 1
+
+libraries.sort(key=lambda x: x['signupDays'] / (x['sumOfUniqueBookScores']
+                                                * x['shippingPerDay']))
 
 # output variables
 signedUpLibraries = []
@@ -62,19 +90,17 @@ for i, library in enumerate(libraries):
 # print(libraries)
 
 outFile = open(f"{fileName}_out.txt", "w+")
+outFile.truncate(0)
 outFile.write(f"{len(signedUpLibraries)}\n")
-# print(len(signedUpLibraries))
 for libraryID in signedUpLibraries:
     booksScanned = booksScannedFromLibraries[libraryID]
     outFile.write(f"{libraryID} {len(booksScanned)}\n")
-    # print(libraryID, len(booksScanned))
-    for i,book in enumerate(booksScanned):
+    for i, book in enumerate(booksScanned):
         if i == len(booksScanned)-1:
             outFile.write(f"{book}\n")
         else:
             outFile.write(f"{book} ")
         score += bookScores[book]
-    print(*booksScanned)
 
 scoreFile.write(str(score))
 f.close()
